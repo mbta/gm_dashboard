1ed9a62829f9d3ee0311b76d0d43617181ee4ebf
Paul Swartz <pswartz@mbta.com>
2025-03-19 09:27:03 -0400

    ci: deploy Next.js site to Pages

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
+const isActions = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  basePath: isProd ? "/gm_dashboard" : "",
-  assetPrefix: isProd ? "/gm_dashboard/" : "",
   images: {
     unoptimized: true,
   },
