b25431eab93865e5dd1c8af4a1be1e45f8c3e92a
Manish Kadiyala <mkadiyala@CTD25309MKADIYA.local>
2025-03-19 09:30:16 -0400

    Fix: update basePath

--- src/components/TransitFilters.tsx
+++ src/components/TransitFilters.tsx
@@ -73,7 +73,7 @@ export default function TransitFilters({ activeFilters, toggleCategory, toggleLi
             {SUBWAY_LINES.map((line) => (
               <Image
                 key={line}
-                src={`/icons/icon-${line.toLowerCase()}-line-default.svg`}
+                src={`icons/icon-${line.toLowerCase()}-line-default.svg`}
                 alt={`${line} line icon`}
                 className={`h-8 w-8 flex transition-all duration-300 cursor-pointer max-w-[40px] max-h-[40px] object-contain ${
                   !activeFilters[line] ? "opacity-50" : "opacity-100"
@@ -92,7 +92,7 @@ export default function TransitFilters({ activeFilters, toggleCategory, toggleLi
             onClick={() => toggleCategory("subway")}
             >
             <Image 
-                src={`/icons/icon-mode-subway-small.svg`} 
+                src={`icons/icon-mode-subway-small.svg`} 
                 alt="subway-icon" 
                 className="w-8 h-8 max-w-[40px] max-h-[40px] object-contain"
                 width={40}
@@ -122,11 +122,11 @@ export default function TransitFilters({ activeFilters, toggleCategory, toggleLi
                 // Handle Green Line segments (Green-B, Green-C, etc.)
                 if (line.includes("Green")) {
                     const shortLine = line.split("-")[1]; // Extract "B", "C", "D", "E"
-                    srcPath = `/icons/icon-green-line-${shortLine.toLowerCase()}-default.svg`;
+                    srcPath = `icons/icon-green-line-${shortLine.toLowerCase()}-default.svg`;
                 } 
                 // Handle Mattapan separately (without 'green-line')
                 else if (line === "Mattapan") {
-                    srcPath = `/icons/icon-mattapan-line-default.svg`;
+                    srcPath = `icons/icon-mattapan-line-default.svg`;
                 }
 
                 return (
@@ -154,7 +154,7 @@ export default function TransitFilters({ activeFilters, toggleCategory, toggleLi
           onClick={() => toggleCategory("lightrail")}
         >
           <Image
-                src={`/icons/icon-mode-trolley-small.svg`} 
+                src={`icons/icon-mode-trolley-small.svg`} 
                 alt="trolley-icon" 
                 className="w-8 h-8 max-w-[40px] max-h-[40px] object-contain"
                 width={40}
@@ -188,7 +188,7 @@ export default function TransitFilters({ activeFilters, toggleCategory, toggleLi
           onClick={() => toggleCategory("commuter")}
         >
           <Image
-                src={`/icons/icon-mode-commuter-rail-small.svg`} 
+                src={`icons/icon-mode-commuter-rail-small.svg`} 
                 alt="commuter-icon" 
                 className="w-8 h-8 max-w-[40px] max-h-[40px] object-contain"
                 width={40}
